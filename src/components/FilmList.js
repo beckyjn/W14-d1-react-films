4d8aa4e9d3b31ad382c40c5432842e3d0b485dbe
beckyjn <rebeccajoynielsen@gmail.come>
2019-05-27 17:14:36 +0100

    app shows film links

---
+++ src/components/FilmList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Film from './Film';
+
+class FilmList extends Component{
+
+  render(){
+    const filmNodes = this.props.data.map( film => {
+      return (
+        <Film name={film.name} key={film.id} url={film.url}></Film>
+      );
+    });
+
+    return (
+      <div className="film-list">
+      <ul>{filmNodes}</ul>
+      </div>
+    )
+  };
+};
+
+export default FilmList;
